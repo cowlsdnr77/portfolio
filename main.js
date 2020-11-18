37af5b8d16022d50611d9f2d1a12ca7b6f609c2a
JInwook823 <53897233+JInwook823@users.noreply.github.com>
2020-11-18 21:08:11 +0900

    CSS: Scroll to section

--- main.js
+++ main.js
@@ -2,14 +2,33 @@
 
 //Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
-const navbarHeight =navbar.getBoundingClientRect().height;
+const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth", block:"center"});
+});
+
+//Handle scrolling when click contact button
+const homeContact = document.querySelector('.home__contact');
+const contact = document.querySelector('#contact');
+
+homeContact.addEventListener('click', () => {   
+    contact.scrollIntoView({behavior: "smooth", block:"center"});
 });
